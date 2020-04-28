a8ac922c8f715889ca798788d50059cd4adef089
Ruthless <ruth@jetpackworkflow.com>
2020-04-27 21:56:00 -0400

    Navigation Icons and Custom Side Drawer

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Main from './components/MainComponent';
 
 export default function App() {
+
+    console.disableYellowBox = true;
+
     return (
         <Main />
     );
