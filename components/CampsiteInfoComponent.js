0e064714b494f89598da79f9deef706d3a81c3f1
Ruthless <ruth@jetpackworkflow.com>
2020-05-12 23:24:14 -0400

    Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -18,6 +18,16 @@ const mapStateToProps = state => {
 const mapDispatchToProps = {
     postFavorite: campsiteId =>(postFavorite(campsiteId)),
     postComment: (campsiteId, rating, author, text) =>(postComment(campsiteId, rating, author, text))
+};
+
+const shareCampsite = (title, message, url) => {
+    Share.share({
+        title: title,
+        message: `${title}: ${message} ${url}`,
+        url: url
+    },{
+        dialogTitle: 'Share ' + title
+    });
 };
 
 function RenderComments({comments}) {
@@ -124,6 +134,15 @@ function RenderCampsite(props) {
                             raised
                             reverse
                             onPress={() => props.onShowModal()}
+                            style={styles.cardItem}
+                        />
+                        <Icon 
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)}
                             style={styles.cardItem}
                         />
                     </View>
